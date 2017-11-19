1035a980deaf7e330cfdb52fd883e725cb77acbc
shaquibkhan <connectshaquib@gmail.com>
2017-11-19 12:41:42 +0530

    commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$(".twentytwenty-container").twentytwenty();
+})
+$(document).ready(function(){
+	$(".btn-warning").click(function(){
+		alert("WANT TO MAKE SOME ORDER");
+	})
+})
